4556cacbfe69db3779bfee4f0895e5d78bf931c4
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-08-09 14:26:58 +0200

    registro usuarios en firestore

--- app/src/main/java/com/marta/bookapp/RegistrarActivity.java
+++ app/src/main/java/com/marta/bookapp/RegistrarActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -13,18 +14,31 @@ import android.widget.Toast;
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrarActivity extends AppCompatActivity {
 
-    EditText  email, password;
+    private static final String TAG = "DocSnippets";
+
+    EditText  email, password, nombre, apellido;
     Button registrar;
+
+    String mail, pass;
+    //name, ape;
 
     FirebaseAuth firebaseAuth;
     AwesomeValidation awesomeValidation;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +46,11 @@ public class RegistrarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registrar);
 
         //setTitle("REGISTRO");
+        registro();
+
+    }
+
+    private void registro(){
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -44,12 +63,15 @@ public class RegistrarActivity extends AppCompatActivity {
 
         email = findViewById(R.id.emailEditText);
         password = findViewById(R.id.passwordEditText);
+        nombre = findViewById(R.id.nombreEditText);
+        apellido = findViewById(R.id.apellidoEditText);
 
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String mail = email.getText().toString();
-                String pass = password.getText().toString();
+                mail = email.getText().toString();
+                pass = password.getText().toString();
+
 
                 if(awesomeValidation.validate()){
                     firebaseAuth.createUserWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -57,6 +79,23 @@ public class RegistrarActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
                                 Toast.makeText(RegistrarActivity.this, "Usuario creado con exito", Toast.LENGTH_SHORT).show();
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("email", mail);
+                                user.put("nombre", nombre.getText().toString());
+                                user.put("apellido", apellido.getText().toString());
+                                db.collection("users").add(user)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+                                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "Error adding document", e);
+                                            }
+                                        });
                                 finish();
                             }else {
                                 String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
@@ -65,13 +104,12 @@ public class RegistrarActivity extends AppCompatActivity {
                         }
                     });
 
+
                 }else{
                     Toast.makeText(RegistrarActivity.this, "Completa todos los datos.", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
-
     }
 
 }
