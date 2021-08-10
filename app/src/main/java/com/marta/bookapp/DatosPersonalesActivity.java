5abf932e64dad71ac290cd6d638c24e9aa4aeb77
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-08-10 14:02:07 +0200

    modificar datos personales

--- app/src/main/java/com/marta/bookapp/DatosPersonalesActivity.java
+++ app/src/main/java/com/marta/bookapp/DatosPersonalesActivity.java
@@ -5,23 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DatosPersonalesActivity extends AppCompatActivity {
 
     TextView mail;
     EditText nombre, apellido;
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Button modificarBTN;
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     SharedPreferences prefs;
     String actualUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_personales);
 
@@ -40,7 +48,25 @@ public class DatosPersonalesActivity extends AppCompatActivity {
             }
         });
 
+        modificarBTN = findViewById(R.id.modificarbutton);
+        modificarBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Map<String, Object> user = new HashMap<>();
+                user.put("nombre", nombre.getText().toString());
+                user.put("apellido", apellido.getText().toString());
+                user.put("email",mail.getText().toString());
+
+                db.collection("users").document(mail.getText().toString()).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(DatosPersonalesActivity.this, "Datos modificados con exito", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
 
     }
 }
