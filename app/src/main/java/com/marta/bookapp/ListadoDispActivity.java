b88ea124acb70a5811fd3208a0ea080f4351b632
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-08-16 22:59:42 +0200

    mostrar libros

--- app/src/main/java/com/marta/bookapp/ListadoDispActivity.java
+++ app/src/main/java/com/marta/bookapp/ListadoDispActivity.java
@@ -1,13 +1,30 @@
 package com.marta.bookapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class ListadoDispActivity extends AppCompatActivity {
 
     TextView cursoTV, claseTV;
+    ListView listaDisponibles;
+    ArrayList<String> listaLibros = new ArrayList<String>();
+    ArrayAdapter <String> arrayAdapter;
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +40,26 @@ public class ListadoDispActivity extends AppCompatActivity {
 
         claseTV = findViewById(R.id.textViewClase);
         claseTV.setText(clase);
+
+        CollectionReference libros = db.collection("libros");
+
+
+        libros.whereEqualTo("Curso", curso).whereEqualTo("Clase", clase).whereEqualTo("Estado", "disponible").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                //listaLibros.add(document.);
+                                System.out.println(document.getId()+" --> "+document.getData());
+                            }
+                        } else {
+                            //Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+        listaDisponibles = findViewById(R.id.lv1);
 
 
     }
