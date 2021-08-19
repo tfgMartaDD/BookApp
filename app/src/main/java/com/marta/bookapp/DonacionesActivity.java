a7760891a03d3df3037837273ed3899f6b63008f
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-08-19 17:16:52 +0200

    donaciones

--- app/src/main/java/com/marta/bookapp/DonacionesActivity.java
+++ app/src/main/java/com/marta/bookapp/DonacionesActivity.java
@@ -2,13 +2,27 @@ package com.marta.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DonacionesActivity extends AppCompatActivity {
+
+    Button addBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donaciones);
+
+        addBTN = findViewById(R.id.addButton);
+        addBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent (DonacionesActivity.this, NuevaDonacionActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
