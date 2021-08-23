f2a40e9e6efa149254c1b711ea8e19f3684f9879
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-08-23 19:12:50 +0200

    cambios

--- app/src/main/java/com/marta/bookapp/CursosActivity.java
+++ app/src/main/java/com/marta/bookapp/CursosActivity.java
@@ -1,17 +1,26 @@
 package com.marta.bookapp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CursosActivity extends AppCompatActivity {
 
     Button pripriBTN, segpriBTN, terpriBTN, cuapriBTN, quipriBTN, sextpriBTN;
     Button priesoBTN, segesoBTN, teresoBTN, cuaesoBTN;
     Button pribachBTN, segbachBTN;
+
+    Button cerrarSesion, menu;
+    SharedPreferences  prefs;
 
 
     @Override
@@ -116,8 +125,30 @@ public class CursosActivity extends AppCompatActivity {
             }
         });
 
+        cerrarSesion = findViewById(R.id.cerrarbutton);
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Borrado de datos del usuario actual
+                prefs  = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.clear();
+                editor.apply();
+
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(CursosActivity.this, "Sesión cerrada correctamente.", Toast.LENGTH_SHORT).show();
+                redirigirAhome();
+            }
+        });
+
     }
 
+    private void redirigirAhome() {
+        Intent i = new Intent (this,MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
 
     private void redirigir(String clase, String curso){
 
