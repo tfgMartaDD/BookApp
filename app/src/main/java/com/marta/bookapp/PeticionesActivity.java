3944230deb53e9d666a831bf0329158e0421e28c
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-08-26 00:15:55 +0200

    peticiones

--- app/src/main/java/com/marta/bookapp/PeticionesActivity.java
+++ app/src/main/java/com/marta/bookapp/PeticionesActivity.java
@@ -26,9 +26,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class PeticionesActivity extends AppCompatActivity {
 
