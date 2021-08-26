0e63b329f370b934e4389c04076a9438e3ddc036
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-08-27 01:20:23 +0200

    admin

--- app/src/main/java/com/marta/bookapp/AdminMenuActivity.java
+++ app/src/main/java/com/marta/bookapp/AdminMenuActivity.java
@@ -1,13 +1,51 @@
 package com.marta.bookapp;
 
+import static com.marta.bookapp.BotonesComunes.cerrarSesion;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminMenuActivity extends AppCompatActivity {
+
+    Button peticionesBTN, donacionesBTN, prestamosBTN, librosBTN;
+    Button cerrarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_menu);
+
+        librosBTN = findViewById(R.id.listalibros);
+        librosBTN.setOnClickListener( (View v) -> {
+            Intent in = new Intent (AdminMenuActivity.this, AdminLibrosActivity.class);
+            startActivity(in);
+        });
+
+        peticionesBTN = findViewById(R.id.peticionesPendientes);
+        peticionesBTN.setOnClickListener( (View v) -> {
+            Intent in = new Intent (AdminMenuActivity.this, AdminPeticionesActivity.class);
+            startActivity(in);
+        });
+
+        prestamosBTN = findViewById(R.id.prestamosActuales);
+        prestamosBTN.setOnClickListener( (View v) -> {
+            Intent in = new Intent (AdminMenuActivity.this, AdminPrestamosActivity.class);
+            startActivity(in);
+        });
+
+        donacionesBTN = findViewById(R.id.donacionesAprobar);
+        donacionesBTN.setOnClickListener( (View v) -> {
+            Intent in = new Intent (AdminMenuActivity.this, AdminDonacionesActivity.class);
+            startActivity(in);
+        });
+
+
+        cerrarSesion = findViewById(R.id.cerrarbutton);
+        cerrarSesion.setOnClickListener( (View v) -> cerrarSesion(AdminMenuActivity.this));
+
     }
 }
--- app/src/main/java/com/marta/bookapp/AdminPrestamosActivity.java
+++ app/src/main/java/com/marta/bookapp/AdminPrestamosActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AdminPrestamoActivity extends AppCompatActivity {
+public class AdminPrestamosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_prestamo);
+        setContentView(R.layout.activity_admin_prestamos);
     }
 }
