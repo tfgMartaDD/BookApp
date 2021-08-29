f0ab78723cfd58b5c8b20104493fadaaac632bb9
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-08-29 21:23:39 +0200

    prestamos

--- app/src/main/java/com/marta/bookapp/AdminPrestamosActivity.java
+++ app/src/main/java/com/marta/bookapp/AdminPrestamosActivity.java
@@ -3,12 +3,51 @@ package com.marta.bookapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminPrestamosActivity extends AppCompatActivity {
+
+    PrestAdminAdapter adapter;
+    ListView listViewAdminPrestamos;
+    List<Prestamo> listaPrestamos = new ArrayList<>();
+
+    Spinner spinner1;
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_prestamos);
+
+        listaPrestamos = obtenerPrestamosUsuarios();
+        listViewAdminPrestamos = findViewById(R.id.lvPrestamosAdmin);
+
+        spinner1 = findViewById(R.id.spinner1);
+
+
+        String [] opciones2 = {"PRIMERO PRIMARIA", "SEGUNDO PRIMARIA", "TERCERO PRIMARIA", "CUARTO PRIMARIA", "QUINTO PRIMARIA", "SEXTO PRIMARIA",
+        "PRIMERO ESO", "SEGUNDO ESO", "TERCERO ESO", "CUARTO ESO", "PRIMERO BACHILLERATO", "SEGUNDO BACHILLERATO"};
+
+
+    }
+
+
+    public List<Prestamo> obtenerPrestamosUsuarios(){
+        List<Prestamo> lista = new ArrayList<>();
+
+        return lista;
+    }
+
+    public List<Prestamo> obtenerPrestamosClases(){
+        List<Prestamo> lista = new ArrayList<>();
+
+        return lista;
     }
 }
--- app/src/main/java/com/marta/bookapp/PrestAdminAdapter.java
+++ app/src/main/java/com/marta/bookapp/PrestAdminAdapter.java
@@ -1,4 +1,70 @@
 package com.marta.bookapp;
 
-public class PrestAdminAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PrestAdminAdapter extends BaseAdapter {
+
+    Context context;
+    List<Prestamo> lst;
+
+    public PrestAdminAdapter(Context context, List<Prestamo> lst) {
+        this.context = context;
+        this.lst = lst;
+    }
+
+    @Override
+    public int getCount() {
+        return lst.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        TextView usuarioTV;
+        TextView asigTV;
+        TextView clasecursoTV;
+        TextView fechaTV;
+        TextView fechaDevTV;
+
+        Prestamo p = lst.get(position);
+        Libro l = p.getLibro();
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.listview_prestamos, null);
+        }
+
+        usuarioTV = convertView.findViewById(R.id.usuarioAdmPres);
+        asigTV = convertView.findViewById(R.id.asigAdmPres);
+        clasecursoTV = convertView.findViewById(R.id.claseAdmPres);
+        fechaTV = convertView.findViewById(R.id.fechaAdmPres);
+        fechaDevTV = convertView.findViewById(R.id.fechaDevAdmPres);
+
+        usuarioTV.setText(p.getUsuario());
+        asigTV.setText(l.getAsignatura());
+
+        String clasecurso = l.getClase() + " " + l.getCurso();
+        clasecursoTV.setText(clasecurso);
+
+        fechaTV.setText(p.getFecha().toString());
+        fechaDevTV.setText(p.getFechaDev().toString());
+
+        return convertView;
+    }
 }
