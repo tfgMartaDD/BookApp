01b739a2bfb6e34f11971da3bec537379fefe450
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-08-29 22:06:02 +0200

    prestamos

--- app/src/main/java/com/marta/bookapp/AdminPrestamosActivity.java
+++ app/src/main/java/com/marta/bookapp/AdminPrestamosActivity.java
@@ -3,7 +3,10 @@ package com.marta.bookapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -18,6 +21,7 @@ public class AdminPrestamosActivity extends AppCompatActivity {
     List<Prestamo> listaPrestamos = new ArrayList<>();
 
     Spinner spinner1;
+    RadioButton rbu, rbc;
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -26,28 +30,48 @@ public class AdminPrestamosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_prestamos);
 
-        listaPrestamos = obtenerPrestamosUsuarios();
         listViewAdminPrestamos = findViewById(R.id.lvPrestamosAdmin);
+
+        rbu = findViewById(R.id.rbUsuario);
+        rbc = findViewById(R.id.rbClase);
 
         spinner1 = findViewById(R.id.spinner1);
 
+        if(rbu.isChecked()){
+            System.out.println("user");
+            listaPrestamos = obtenerPrestamosUsuarios();
 
-        String [] opciones2 = {"PRIMERO PRIMARIA", "SEGUNDO PRIMARIA", "TERCERO PRIMARIA", "CUARTO PRIMARIA", "QUINTO PRIMARIA", "SEXTO PRIMARIA",
-        "PRIMERO ESO", "SEGUNDO ESO", "TERCERO ESO", "CUARTO ESO", "PRIMERO BACHILLERATO", "SEGUNDO BACHILLERATO"};
+        }else if(rbc.isChecked()){
+            System.out.println("spinner");
+            spinner1.setVisibility(View.VISIBLE);
+
+            String [] opciones = {"PRIMERO PRIMARIA", "SEGUNDO PRIMARIA", "TERCERO PRIMARIA", "CUARTO PRIMARIA", "QUINTO PRIMARIA", "SEXTO PRIMARIA",
+                    "PRIMERO ESO", "SEGUNDO ESO", "TERCERO ESO", "CUARTO ESO", "PRIMERO BACHILLERATO", "SEGUNDO BACHILLERATO"};
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
+            spinner1.setAdapter(arrayAdapter);
+
+            String clasecurso = spinner1.getSelectedItem().toString();
+
+            listaPrestamos = obtenerPrestamosClases(clasecurso);
+        }
+
+        adapter = new PrestAdminAdapter(this, listaPrestamos);
+        listViewAdminPrestamos.setAdapter(adapter);
 
 
     }
 
 
     public List<Prestamo> obtenerPrestamosUsuarios(){
-        List<Prestamo> lista = new ArrayList<>();
+        List<Prestamo> listaUsuarios = new ArrayList<>();
 
-        return lista;
+        return listaUsuarios;
     }
 
-    public List<Prestamo> obtenerPrestamosClases(){
-        List<Prestamo> lista = new ArrayList<>();
+    public List<Prestamo> obtenerPrestamosClases(String clase){
+        List<Prestamo> listaClases = new ArrayList<>();
 
-        return lista;
+        return listaClases;
     }
 }
