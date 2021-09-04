7f62ef96d55277b44471c1d4e5e3da5f40b414ff
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-09-04 19:51:38 +0200

    libro escaneado

--- app/src/main/java/com/marta/bookapp/BusquedaActivity.java
+++ app/src/main/java/com/marta/bookapp/BusquedaActivity.java
@@ -3,8 +3,19 @@ package com.marta.bookapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 public class BusquedaActivity extends AppCompatActivity {
+
+    TextView asignatura, clase, curso, editorial;
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +24,24 @@ public class BusquedaActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         String id = bundle.getString("id");
+
+        asignatura = findViewById(R.id.busqAsig);
+        clase = findViewById(R.id.busqClase);
+        curso = findViewById(R.id.busqCurso);
+        editorial = findViewById(R.id.busqEdit);
+
+        db.collection("libros").document(id).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Libro libro = new Libro(documentSnapshot.getId(), documentSnapshot.getString("Asignatura"), documentSnapshot.getString("Clase"), documentSnapshot.getString("Curso"),
+                        documentSnapshot.getString("Donante"), documentSnapshot.getString("Editorial"), documentSnapshot.getString("Estado"), documentSnapshot.getString("Imagen"), documentSnapshot.getString("Tipo") );
+
+                asignatura.setText(documentSnapshot.getString("Asignatura"));
+                clase.setText(documentSnapshot.getString("Clase"));
+                curso.setText(documentSnapshot.getString("Curso"));
+                editorial.setText(documentSnapshot.getString("Editorial"));
+            }
+        });
+
     }
 }
