3d4b2f87cc4ca28d42b4706ba98c418e2defd4b6
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-09-06 05:10:37 +0200

    cambios

--- app/src/main/java/com/marta/bookapp/BotonesComunes.java
+++ app/src/main/java/com/marta/bookapp/BotonesComunes.java
@@ -38,7 +38,11 @@ public class BotonesComunes {
     }
 
     public static void volverAMenuAdmin(Context context){
+        SharedPreferences prefs = context.getSharedPreferences(prefs_file, Context.MODE_PRIVATE);
+        String email = prefs.getString("email","");
+
         Intent in = new Intent (context,AdminMenuActivity.class);
+        in.putExtra("email",email);
         context.startActivity(in);
     }
 
