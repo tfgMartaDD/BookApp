992b39f706c07d86511f734d7f1a7d7283ca658c
tfgMartaDD <88137465+tfgMartaDD@users.noreply.github.com>
2021-12-29 20:04:11 +0100

    libros y donaciones

--- app/src/main/java/com/marta/bookapp/Activitys/NuevaDonacionActivity.java
+++ app/src/main/java/com/marta/bookapp/Activitys/NuevaDonacionActivity.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -53,6 +54,12 @@ public class NuevaDonacionActivity extends AppCompatActivity {
     RadioButton defectoRB, galeriaRB;
     LinearLayout llimagen;
 
+    RadioButton listarRB, rellenarRB;
+    LinearLayout llListar, llRellenar;
+    EditText asignaturaET, claseET, cursoET, editorialET;
+
+    Boolean flag = true;
+
     private StorageReference mStorage;
     Uri uriImagen;
 
@@ -87,6 +94,17 @@ public class NuevaDonacionActivity extends AppCompatActivity {
 
         anadirImagen = findViewById(R.id.anadirBTN);
 
+        listarRB = findViewById(R.id.rbListar);
+        rellenarRB = findViewById(R.id.rbRellenar);
+
+        llListar = findViewById(R.id.agrupacionListar);
+        llRellenar = findViewById(R.id.agrupacionRellenar);
+
+        asignaturaET = findViewById(R.id.asigET);
+        claseET = findViewById(R.id.claseET);
+        cursoET = findViewById(R.id.cursoET);
+        editorialET = findViewById(R.id.editorialET);
+
         String [] asignaturas = {"MATEMATICAS", "LENGUA", "BIOLOGIA", "SOCIALES", "INGLES", "FRANCES", "EDUCACION FISICA",
                 "PLASTICA", "QUIMICA", "NATURALES", "LATIN"};
         ArrayAdapter<String> asigAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, asignaturas);
@@ -107,24 +125,89 @@ public class NuevaDonacionActivity extends AppCompatActivity {
 
         donarBTN = findViewById(R.id.donarButton);
         donarBTN.setOnClickListener( (View v) -> {
-            String claseTemp = claseSpin.getSelectedItem().toString();
-            String cursoTemp = cursoSpin.getSelectedItem().toString();
 
-            if( (claseTemp.equalsIgnoreCase("QUINTO") && ((cursoTemp.equalsIgnoreCase("ESO")||((cursoTemp.equalsIgnoreCase("BACHILLERATO"))))))
-                    || ((claseTemp.equalsIgnoreCase("SEXTO"))&&((cursoTemp.equalsIgnoreCase("ESO")||((cursoTemp.equalsIgnoreCase("BACHILLERATO"))))))
-            || ((cursoTemp.equalsIgnoreCase("BACHILLERATO")) && ((claseTemp.equalsIgnoreCase("TERCERO")) || (claseTemp.equalsIgnoreCase("CUARTO"))))){
-                Toast.makeText(NuevaDonacionActivity.this,"DEBE SELECCIONAR UNA CLASE Y CURSOS VÁLIDOS.",Toast.LENGTH_LONG).show();
+            if(flag){
+                String claseTemp = claseSpin.getSelectedItem().toString();
+                String cursoTemp = cursoSpin.getSelectedItem().toString();
 
-            }else {
-                String asignatura = asigSpin.getSelectedItem().toString();
-                String clase = claseSpin.getSelectedItem().toString();
-                String curso = cursoSpin.getSelectedItem().toString();
-                String editorial = editorialSpin.getSelectedItem().toString();
+                if( (claseTemp.equalsIgnoreCase("QUINTO") && ((cursoTemp.equalsIgnoreCase("ESO")||((cursoTemp.equalsIgnoreCase("BACHILLERATO"))))))
+                        || ((claseTemp.equalsIgnoreCase("SEXTO"))&&((cursoTemp.equalsIgnoreCase("ESO")||((cursoTemp.equalsIgnoreCase("BACHILLERATO"))))))
+                || ((cursoTemp.equalsIgnoreCase("BACHILLERATO")) && ((claseTemp.equalsIgnoreCase("TERCERO")) || (claseTemp.equalsIgnoreCase("CUARTO"))))){
+                    Toast.makeText(NuevaDonacionActivity.this,"DEBE SELECCIONAR UNA CLASE Y CURSOS VÁLIDOS.",Toast.LENGTH_LONG).show();
+
+                }else {
+                    String asignatura = asigSpin.getSelectedItem().toString();
+                    String clase = claseSpin.getSelectedItem().toString();
+                    String curso = cursoSpin.getSelectedItem().toString();
+                    String editorial = editorialSpin.getSelectedItem().toString();
+
+                    prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE);
+                    String actualUser = prefs.getString("email", "");
+
+                    String frase = "¿Está seguro de que quiere donar el libro de la asignatura " + asignatura + " del curso " + clase + " " + curso + " de la editorial " + editorial + "?";
+
+                    AlertDialog.Builder alerta = new AlertDialog.Builder(NuevaDonacionActivity.this);
+                    alerta.setMessage(frase).setPositiveButton("SI", (DialogInterface dialog, int id) -> {
+
+                        Date date = new Date();
+
+                        Map<String, Object> pendiente = new HashMap<>();
+                        pendiente.put("Asignatura", asignatura);
+                        pendiente.put("esPeticion","false");
+                        pendiente.put("Clase",clase);
+                        pendiente.put("Curso",curso);
+                        pendiente.put("Estado", "Pendiente");
+                        pendiente.put("Usuario",actualUser);
+
+                        db.collection("pendientes").add(pendiente).addOnSuccessListener( (DocumentReference documentReference) ->{
+                            idPendiente = documentReference.getId();
+
+                            Map<String, Object> donation = new HashMap<>();
+                            donation.put("Asignatura", asignatura);
+                            donation.put("Clase", clase);
+                            donation.put("Curso", curso);
+                            donation.put("Editorial", editorial);
+                            donation.put("Usuario", actualUser);
+                            donation.put("Fecha", date);
+                            donation.put("idPendiente", idPendiente);
+                            if(urlImagen == null){
+                                StorageReference carpeta = mStorage.child("portadas").child("donaciones");
+                                StorageReference filePath = carpeta.child(uriImagen.getLastPathSegment());
+                                filePath.putFile(uriImagen).addOnSuccessListener(taskSnapshot -> filePath.getDownloadUrl().addOnSuccessListener( uri -> {
+                                    urlImagen = String.valueOf(uri);
+                                    donation.put("Imagen", urlImagen);
+
+                                    db.collection("posiblesDonaciones").document().set(donation).addOnSuccessListener((Void unused) -> {
+                                        Toast.makeText(NuevaDonacionActivity.this, "DONACION RECIBIDA.\nLos administradores tienen que aprobar la donación. Se pondran en contacto con usted en breve. ", Toast.LENGTH_LONG).show();
+                                        volverAMenu(NuevaDonacionActivity.this);
+                                    });
+                                }));
+                            }else{
+                                donation.put("Imagen", urlImagen);
+
+                                db.collection("posiblesDonaciones").document().set(donation).addOnSuccessListener((Void unused) -> {
+                                    Toast.makeText(NuevaDonacionActivity.this, "DONACION RECIBIDA.\nLos administradores tienen que aprobar la donación. Se pondran en contacto con usted en breve. ", Toast.LENGTH_LONG).show();
+                                    volverAMenu(NuevaDonacionActivity.this);
+                                });
+                            }
+                        });
+
+                    }).setNegativeButton("NO", (DialogInterface dialog, int id) -> Toast.makeText(NuevaDonacionActivity.this, "DONACION CANCELADA", Toast.LENGTH_SHORT).show());
+
+                    AlertDialog alertDialog = alerta.create();
+                    alertDialog.setTitle("¿ESTAS SEGURO?");
+                    alertDialog.show();
+                }
+            }else{
+                String asignaturaR = String.valueOf(asignaturaET.getText());
+                String claseR = String.valueOf(claseET.getText());
+                String cursoR = String.valueOf(cursoET.getText());
+                String editorialR = String.valueOf(editorialET.getText());
 
                 prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE);
                 String actualUser = prefs.getString("email", "");
 
-                String frase = "¿Está seguro de que quiere donar el libro de la asignatura " + asignatura + " del curso " + clase + " " + curso + " de la editorial " + editorial + "?";
+                String frase = "¿Está seguro de que quiere donar el libro de la asignatura " + asignaturaR + " del curso " + claseR + " " + cursoR + " de la editorial " + editorialR + "?";
 
                 AlertDialog.Builder alerta = new AlertDialog.Builder(NuevaDonacionActivity.this);
                 alerta.setMessage(frase).setPositiveButton("SI", (DialogInterface dialog, int id) -> {
@@ -132,10 +215,10 @@ public class NuevaDonacionActivity extends AppCompatActivity {
                     Date date = new Date();
 
                     Map<String, Object> pendiente = new HashMap<>();
-                    pendiente.put("Asignatura", asignatura);
+                    pendiente.put("Asignatura", asignaturaR);
                     pendiente.put("esPeticion","false");
-                    pendiente.put("Clase",clase);
-                    pendiente.put("Curso",curso);
+                    pendiente.put("Clase",claseR);
+                    pendiente.put("Curso",cursoR);
                     pendiente.put("Estado", "Pendiente");
                     pendiente.put("Usuario",actualUser);
 
@@ -143,10 +226,10 @@ public class NuevaDonacionActivity extends AppCompatActivity {
                         idPendiente = documentReference.getId();
 
                         Map<String, Object> donation = new HashMap<>();
-                        donation.put("Asignatura", asignatura);
-                        donation.put("Clase", clase);
-                        donation.put("Curso", curso);
-                        donation.put("Editorial", editorial);
+                        donation.put("Asignatura", asignaturaR);
+                        donation.put("Clase", claseR);
+                        donation.put("Curso", cursoR);
+                        donation.put("Editorial", editorialR);
                         donation.put("Usuario", actualUser);
                         donation.put("Fecha", date);
                         donation.put("idPendiente", idPendiente);
@@ -205,6 +288,19 @@ public class NuevaDonacionActivity extends AppCompatActivity {
         }else if(galeriaRB.isChecked()){
             anadirImagen.setVisibility(View.VISIBLE);
             donarBTN.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void comprobarRBDonacion(View view){
+        if(rellenarRB.isChecked()){
+            llRellenar.setVisibility(View.VISIBLE);
+            llListar.setVisibility(View.INVISIBLE);
+            flag = false;
+
+        }else if(listarRB.isChecked()){
+            llRellenar.setVisibility(View.INVISIBLE);
+            llListar.setVisibility(View.VISIBLE);
+            flag = true;
         }
     }
 
